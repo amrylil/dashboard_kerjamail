2a7e9d854367ff7871b178a1cbdb594a35d3347a
AmryLil <ulilamry432@gmail.com>
2025-09-24 22:03:38 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-red-700 font-bold text-3xl">Tiaa Sayangg</h1>
+    </div>
+  );
+}
+
+export default App;
